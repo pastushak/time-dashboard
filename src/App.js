d5c70d55f7c8a461996f88d9c37488252d6d2033
pastushak <pastushak_roman@kolgym.if.ua>
2025-04-28 22:34:28 +0300

    Add files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold text-center mt-10">Візуалізатор робочого часу</h1>
+    </div>
+  );
+}
+
+export default App;
